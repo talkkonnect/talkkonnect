c5e1e9232e66aa3be40c90d5e782d72aa6cb6ad7
Suvir Kumar <suvir@talkkonnect.com>
2019-02-07 17:44:30 +0700

    added copyright

--- cmd/talkkonnect/main.go
+++ cmd/talkkonnect/main.go
@@ -1,3 +1,33 @@
+/*
+ * talkkonnect headless mumble client/gateway with lcd screen and channel control
+ * Copyright (C) 2018-2019, Suvir Kumar <suvir@talkkonnect.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * talkkonnect is the based on talkiepi and barnard
+ *
+ * The Initial Developer of the Original Code is
+ * Suvir Kumar <suvir@talkkonnect.com>
+ * Portions created by the Initial Developer are Copyright (C)
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Suvir Kumar <suvir@talkkonnect.com>
+ * My Blog is at www.talkkonnect.com
+ * The source code is hosted at github.com/talkkonnect
+ *
+ * talkkonnect.go -- Main Function
+ *
+ */
+
 package main
 
 import (
@@ -9,12 +39,9 @@ import (
 
 func main() {
 
-	// Command line flags
-
 	config := flag.String("config", "/home/talkkonnect/gocode/src/github.com/talkkonnect/talkkonnect/talkkonnect.xml", "full path to talkkonnect.xml configuration file")
 
 	flag.Usage = talkkonnectusage
-
 	flag.Parse()
 
 	log.Println("info: Reading Config File: ", *config)
@@ -27,7 +54,7 @@ func talkkonnectusage() {
 	fmt.Println("By Suvir Kumar <suvir@talkkonnect.com>")
 	fmt.Println("For more information visit http://www.talkkonnect.com and github.com/talkkonnect")
 	fmt.Println("---------------------------------------------------------------------------------------")
-	fmt.Println("-config=/home/mumble/gocode/src/github.com/talkkonnect/talkkonnect/talkkonnect.xml")
+	fmt.Println("-config=/home/talkkonnect/gocode/src/github.com/talkkonnect/talkkonnect/talkkonnect.xml")
 	fmt.Println("-version for the version")
 	fmt.Println("-help for this screen")
 }

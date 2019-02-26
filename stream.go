aa1c8c960a6df0d7f045fd9688c0fd54014e683d
root <root@sugriva.dits.co.th>
2019-02-26 17:06:12 +0700

    removed delay in stream.go

--- stream.go
+++ stream.go
@@ -135,7 +135,7 @@ func (s *Stream) StopSource() error {
 	s.deviceSource.CaptureStop()
 
 	// on stop source adjust from 100ms to 25 ms helps reduce recovery time and helps with audio chopping (suvir kumar)
-	time.Sleep(25 * time.Millisecond)
+	//time.Sleep(25 * time.Millisecond)
 
 	s.deviceSource.CaptureCloseDevice()
 	s.deviceSource = nil

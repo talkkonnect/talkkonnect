68abe79da2a5882fb4211f345aba406c57416295
talkkonnect <suvir@talkkonnect.com>
2019-03-17 13:57:38 +0700

    upped version

--- talkkonnect.go
+++ talkkonnect.go
@@ -50,7 +50,7 @@ func talkkonnectBanner() {
 	log.Println("info: ├────────────────────────────────────────────────────────────────┤")
 	log.Println("info: │Created By : Suvir Kumar  <suvir@talkkonnect.com>               │")
 	log.Println("info: ├────────────────────────────────────────────────────────────────┤")
-	log.Println("info: │Version 1.45 Released March 17 2019                             │")
+	log.Println("info: │Version 1.46 Released March 17 2019                             │")
 	log.Println("info: │Additional Modifications Released under MPL 2.0 License         │")
 	log.Println("info: ├────────────────────────────────────────────────────────────────┤")
 	log.Println("info: │visit us at www.talkkonnect.com and github.com/talkkonnect      │")

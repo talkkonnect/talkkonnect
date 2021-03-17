74df0b811670c608bd81b2da4a2221bf8dce3348
talkkonnect <suvir@talkkonnect.com>
2021-03-17 06:50:33 +0000

    added license header in file

--- ledstrip.go
+++ ledstrip.go
@@ -1,3 +1,34 @@
+/*
+ * talkkonnect headless mumble client/gateway with lcd screen and channel control
+ * Copyright (C) 2018-2019, Suvir Kumar <suvir@talkkonnect.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * talkkonnect is the based on talkiepi and barnard by Daniel Chote and Tim Cooper
+ *
+ * The source code for ledstrip was copied from github.com/CustomMachines and the code was written by Ben Lewis licensed under MPL Version 2 License
+ * The Initial Developer of the Original Code is
+ * Ben Lewis
+ *
+ * Contributor(s):
+ *
+ * Ben Lewis
+ * Suvir Kumar <suvir@talkkonnect.com>
+ *
+ * My Blog is at www.talkkonnect.com
+ * The source code is hosted at github.com/talkkonnect
+ *
+ * ledstrip.go -> function in talkkonnect to control the led strip on respeaker hat
+ */
+
+
 package talkkonnect
 
 import (

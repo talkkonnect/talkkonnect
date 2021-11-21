a522256436471ea2df415820e2a6bfe16540a498
Suvir Kumar <suvir@talkkonnect.com>
2021-11-21 19:02:16 +0700

    Update tkbuild.sh

--- scripts/tkbuild.sh
+++ scripts/tkbuild.sh
@@ -37,7 +37,7 @@ adduser --disabled-password --disabled-login --gecos "" talkkonnect
 usermod -a -G cdrom,audio,video,plugdev,users,dialout,dip,input,gpio talkkonnect
 
 ## Install the dependencies required for talkkonnect
-apt-get -y install libopenal-dev libopus-dev libasound2-dev git ffmpeg omxplayer screen
+apt-get -y install libopenal-dev libopus-dev libasound2-dev git ffmpeg mplayer screen
 
 ## Create the necessary directory structure under /home/talkkonnect/
 cd /home/talkkonnect/
@@ -48,8 +48,8 @@ mkdir -p /home/talkkonnect/bin
 touch /var/log/talkkonnect.log
 
 cd /usr/local
-wget https://golang.org/dl/go1.17.2.linux-armv6l.tar.gz
-tar -zxvf go1.17.2.linux-armv6l.tar.gz
+wget https://golang.org/dl/go1.17.3.linux-armv6l.tar.gz
+tar -zxvf go1.17.3.linux-armv6l.tar.gz
 
 echo export PATH=$PATH:/usr/local/go/bin >>  ~/.bashrc
 echo export GOPATH=/home/talkkonnect/gocode >>  ~/.bashrc

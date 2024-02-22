544c673d002e648a53dd53c5d91b0f58edb1b5bf
Suvir Kumar <suvir@talkkonnect.com>
2024-02-22 10:11:04 +0700

    Update update-talkkonnect.sh
    fixed bug when creating directory

--- scripts/update-talkkonnect.sh
+++ scripts/update-talkkonnect.sh
@@ -95,7 +95,7 @@ export GOBIN=/home/talkkonnect/bin
 ## Get the latest source code of talkkonnect from github.com
 echo "installing talkkonnect with go installing"
 cd $GOPATH
-mkdir /home/talkkonnect/gocode/src/github.com/talkkonnect
+mkdir -p /home/talkkonnect/gocode/src/github.com/talkkonnect
 cd /home/talkkonnect/gocode/src/github.com/talkkonnect
 git clone https://github.com/talkkonnect/talkkonnect
 cd /home/talkkonnect/gocode/src/github.com/talkkonnect/talkkonnect
